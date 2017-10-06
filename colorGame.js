5b40d4c26a4d9c08a0d9d49e5c90cab1fc457c82
Jherey <jerry4christ2004@yahoo.com>
2017-10-06 21:37:06 +0100

    Add Event Listeners to each square

--- colorGame.js
+++ colorGame.js
@@ -10,10 +10,33 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
 	//add initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked square
+		var clickedColor = this.style.backgroundColor;
+		//compare color to pickedColor
+		if (clickedColor === pickedColor) {
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
+		} else {
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	});
+}
+
+function changeColors(color) {
+	//loop through all squares
+	for(var i =0; i < squares.length; i++) {
+		//change each color to match each color
+		squares[i].style.backgroundColor = color;
+	}
 }
