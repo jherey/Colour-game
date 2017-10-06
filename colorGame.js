3088d7eaf3de89ef70e5869cc7aa84653598963e
Jherey <jerry4christ2004@yahoo.com>
2017-10-06 22:34:42 +0100

    Change h1 color to clicked/correct color

--- colorGame.js
+++ colorGame.js
@@ -1,9 +1,9 @@
 var colors = generateRandomColors(6);
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,6 +19,7 @@ for(var i = 0; i < squares.length; i++){
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		} else {
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
