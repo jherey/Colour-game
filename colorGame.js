3e9a4c955bae41748d396abf114e1d29f04ddbb9
Jherey <jerry4christ2004@yahoo.com>
2017-10-06 22:30:50 +0100

    Add random colors to squares

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,7 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255",
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 
@@ -39,4 +32,32 @@ function changeColors(color) {
 		//change each color to match each color
 		squares[i].style.backgroundColor = color;
 	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num) {
+	//make an array
+	var arr = [];
+	//repeat num times
+	for(var i = 0; i < num; i++) {
+		//get random color and push into array
+		arr.push(randomColor());
+	}
+	//return that arrsy
+	return arr;
+}
+
+function randomColor(){
+	//pick a "red" from 0 - 255;
+	var r = Math.floor(Math.random() * 256);
+	//pick a "green" from 0 - 255;
+	var g = Math.floor(Math.random() * 256);
+	//pick a "blue" from 0 - 255;
+	var b = Math.floor(Math.random() * 256);
+
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
